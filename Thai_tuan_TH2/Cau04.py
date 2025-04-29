9c04df652332bc1b4def336ec97aebebf68a5555
qwertytuan <tuanthai902@gmail.com>
2025-04-29 08:12:30 +0700

    update

--- Thai_tuan_TH2/Cau04.py
+++ Thai_tuan_TH2/Cau04.py
@@ -5,6 +5,7 @@ from Anh import Anh
 img = cv2.imread(Anh)
 img2 = cv2.resize(img, None, fx = 1.5, fy = 1.5)
 cv2.imshow(f'Anh moi = {img.shape[:2]} x1.5', img2)
+print(img2.shape[:2])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
