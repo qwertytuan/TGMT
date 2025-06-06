962a89e44b5a4bcddf83fc5f92698fe8b92ae0ee
qwertytuan <tuanthai902@gmail.com>
2025-06-06 20:24:11 +0700

    Remove video

--- TH_3/Bai2.py
+++ TH_3/Bai2.py
@@ -1,6 +1,11 @@
 import cv2 as cv
 
 cap = cv.VideoCapture('TH_3/bai2.mp4')
+fps = cap.get(cv.CAP_PROP_FPS)
+cap.set(cv.CAP_PROP_HW_ACCELERATION,2)
+length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
+time= int(length/fps)
+print(time)
 
 while True:
     ret, frame = cap.read()
@@ -8,7 +13,7 @@ while True:
         break
 
     
-    if cv.waitKey(23) ==ord('x'):
+    if cv.waitKey(time) ==ord('x'):
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         gray = cv.GaussianBlur(gray, (5, 5), 0)
         laplace= cv.Laplacian(gray, cv.CV_64F, ksize=3)
@@ -16,7 +21,7 @@ while True:
         cv.imwrite('TH_3/laplace.jpg', laplace)
         
     cv.imshow('Original', frame)
-    if cv.waitKey(23) == ord('q'):
+    if cv.waitKey(time) == ord('q'):
         break
 
 cap.release()
